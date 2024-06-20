7a3b329e25580dea4fb7c8dc6be27ec9aadea4c2
Anuj Agrawal <anujagrawal1804@gmail.com>
2024-06-21 00:40:04 +0530

    c

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,22 @@ const nextConfig = {
     reactStrictMode: true,
 };
 
-
+if (process.env.NODE_ENV === "production") {
+    if (process.env.STATIC_ONLY === "true") {
+      nextConfig.pageExtensions = ["page.tsx", "page.ts", "page.jsx", "page.js"];
+      nextConfig.output = "export";
+    } else {
+      nextConfig.pageExtensions = ["api.ts", "api.js"];
+    }
+  } else {
+    nextConfig.pageExtensions = [
+      "page.tsx",
+      "page.ts",
+      "page.jsx",
+      "page.js",
+      "api.ts",
+      "api.js",
+    ];
+  }
+  
 export default nextConfig;
