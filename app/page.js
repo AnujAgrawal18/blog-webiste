b8a2b6dc9a06cfc7f1697b97294a4b6167a1c4dd
Anuj Agrawal <anujagrawal1804@gmail.com>
2024-06-21 21:37:19 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+"use client"
 import Blogs from "@/components/Blogs"
 import Highlight from "@/components/Highlight";
 import Link from "next/link";
--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    
 };
 
-if (process.env.NODE_ENV === "production") {
-    if (process.env.STATIC_ONLY === "true") {
-      nextConfig.pageExtensions = ["page.tsx", "page.ts", "page.jsx", "page.js"];
-      nextConfig.output = "export";
-    } else {
-      nextConfig.pageExtensions = ["api.ts", "api.js"];
-    }
-  } else {
-    nextConfig.pageExtensions = [
-      "page.tsx",
-      "page.ts",
-      "page.jsx",
-      "page.js",
-      "api.ts",
-      "api.js",
-    ];
-  }
+
   
 export default nextConfig;
